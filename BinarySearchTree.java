bfb3ca7e7b0ab4b56c1ad084ce4e42a9cabe3eaa
Horacio <hernandezh4@hotmail.com>
2024-01-09 20:53:15 -0800

    added the following methods: add, search, check to see if it exists(contains), maximum and minimum. Will follow up with a predecessor, successor and remobe method.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -30,11 +30,90 @@ public class BinarySearchTree<T extends Comparable<? super T>>{
     public void print(){
         print(this.root, 0);
         System.out.println("-------");
+
     }
 
+    public boolean Add(T toAdd){
+        Node n = new Node(toAdd);
 
-    //TODO add rest of methods
+        if(root == null){
+            root = n;
+            size++;
+            return true;
+        }
 
+        Node aux = root;
+        Node aux2 = null;
+        int cmp = 0;
+
+        while(aux != null){
+            cmp = aux.key.compareTo(toAdd);
+            aux2 = aux;
+
+            if(cmp == 0)
+                return false;
+            aux = cmp > 0 ? aux.left : aux.right;
+        }
+        if(cmp > 0){
+            aux2.left = n;
+        }else{
+            aux2.right = n;
+        }
+
+        n.parent = aux2;
+        size++;
+
+        return true;
+    }
+
+    private Node search( T toSearch, Node current) {
+        while(current != null){
+            int cmp = current.value.compareTo(toSearch);
+            if(cmp == 0)
+                return current;
+            current = cmp > 0 ? current.left : current.right;
+        }
+        return null;
+    }
+
+    public boolean cotains(T value) {
+        if(value == null)
+            return false;
+
+            return search(value, root) != null;
+    }
+
+    private Node maximum(Node node){
+        if(node == null){
+            return null;
+        }
+        while(node.right != null){
+            node = node.right;
+        }
+        return node;
+    }
+
+    public T maximum(){
+        Node max = maximum(root);
+            return max != null ? max.value : null;
+    }
+
+    private Node minimum(Node node){
+        if(node == null){
+            return null;
+        }
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+
+    public T minimum(){
+        Node min = minimum(root);
+            return min != null ? min.value : null;
+    }
+
+    //TODO add a predecessor, successor and a remove method
 
 
 }
